729378080fce62a0be3ff098f06e7b06f680d342
david-commit <david.ondiege@students.moringaschool.com>
2022-08-17 21:06:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+// Modulus expression
+const num3 = 64;
+const num4 = 6;
+
+const mod = num3 % num4;
+
+//Max Value
+const max = Math.max(5, 6, 2, 20);
+
+//Generates a random number
+const random = Math.floor(Math.random() * 1000) + 1;
+
